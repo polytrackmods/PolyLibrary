fe9a5fd54edf796e8d11622c3e388d152eee03cc
DoraChad <176503791+DoraChad@users.noreply.github.com>
2025-07-27 18:53:50 +0200

    Update main.mod.js

--- 0.1.1/main.mod.js
+++ 0.1.1/main.mod.js
@@ -179,7 +179,7 @@ class polyLibrary extends PolyMod {
         .changelog-list {
             flex: 1;
             display: flex;
-            flex-direction: column;
+            flex-direction: column-reverse;
             align-items: center;
             padding: 20px;
             gap: 40px;
@@ -196,7 +196,7 @@ class polyLibrary extends PolyMod {
             overflow-y: scroll;
             pointer-events: auto;
             display: flex;
-            flex-direction: column-reverse;
+            flex-direction: column;
             gap: 20px;
             padding: 20px;
             flex: 1;
