56f5a01754ea484849b5edd20e198e25b6a7865c
0rangy <86059513+0rangy@users.noreply.github.com>
2025-07-28 20:37:42 +0200

    Fix const

--- 0.1.1/main.mod.js
+++ 0.1.1/main.mod.js
@@ -983,7 +983,7 @@ class polyLibrary extends PolyMod {
     };
 
     addMod = function(modurl, modversion="latest") {
-        const autoUpd = false;
+        let autoUpd = false;
         if (modversion === "latest") {autoUpd = true}
         this.apml.addMod({ base: modurl, version: modversion, loaded: true }, autoUpd)
         .then(mod => {
