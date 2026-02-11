c4c69b5f8f098317f9e2dc320999aae0e0b84758
DoraChad <176503791+DoraChad@users.noreply.github.com>
2026-02-11 11:50:43 -0800

    fixes

--- 0.1.6/main.mod.js
+++ 0.1.6/main.mod.js
@@ -1136,7 +1136,7 @@ class polyLibrary extends PolyMod {
         `);
 
         this.gameVersion = "0.5.2";
-        console.log(this, window.ActivePolyModLoader, window.ActivePolyModLoader.polyVersion);
+        console.log(this, window.ActivePolyModLoader);
 
         let mixinLocation = "mN";
         if (this.gameVersion === "0.5.2") {
